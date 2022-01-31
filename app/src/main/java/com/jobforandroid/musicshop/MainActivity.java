8eeda299b99e664fa4daede23aff9bc72f934979
Galyminsky-PC <jobforandroid@protonmail.com>
2022-02-01 05:23:09 +0600

    4.0

--- app/src/main/java/com/jobforandroid/musicshop/MainActivity.java
+++ app/src/main/java/com/jobforandroid/musicshop/MainActivity.java
@@ -8,16 +8,26 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
-    public void increaseQuantity (View view) {
+    public void increaseQuantity(View view) {
         TextView count_quantity = findViewById(R.id.count_quantity);
-        count_quantity.setText("1");
+        quantity = quantity + 1;
+        count_quantity.setText("" + quantity);
+    }
 
+    public void decraseQuantity(View view) {
+        TextView count_quantity = findViewById(R.id.count_quantity);
+        quantity = quantity - 1;
+        if (quantity < 0) {
+            quantity = 0;
+        }
+        count_quantity.setText("" + quantity);
     }
 }
