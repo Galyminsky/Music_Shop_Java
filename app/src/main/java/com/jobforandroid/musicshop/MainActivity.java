154cb2a765d2e96dfca7c3805eb8bed521b48d71
Galyminsky-PC <jobforandroid@protonmail.com>
2022-01-31 06:01:03 +0600

    1.0

--- app/src/main/java/com/jobforandroid/musicshop/MainActivity.java
+++ app/src/main/java/com/jobforandroid/musicshop/MainActivity.java
@@ -3,6 +3,8 @@ package com.jobforandroid.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void increaseQuantity (View view) {
+        TextView count_quantity = findViewById(R.id.count_quantity);
+        count_quantity.setText("1");
+
     }
 }
