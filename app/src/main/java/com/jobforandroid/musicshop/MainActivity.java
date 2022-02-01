a942a6db354eda4989e884e4dc2724a88ee1acaf
Galyminsky-PC <jobforandroid@protonmail.com>
2022-02-01 06:12:17 +0600

    SpinnerAdapter Completed

--- app/src/main/java/com/jobforandroid/musicshop/MainActivity.java
+++ app/src/main/java/com/jobforandroid/musicshop/MainActivity.java
@@ -4,16 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
+    Spinner spinner;
+    ArrayList spinnerArrayList;
+    ArrayAdapter spinnerAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.main_spinner);
+
+        spinnerArrayList = new ArrayList();
+        spinnerArrayList.add("Guitar");
+        spinnerArrayList.add("Skripka");
+        spinnerArrayList.add("Baraban");
+
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(spinnerAdapter);
+
+
     }
 
     public void increaseQuantity(View view) {
